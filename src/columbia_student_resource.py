c3b859a6000427791d95d3657054fe251b954b95
Juan Angeles <jcangeles@outlook.com>
2022-09-27 22:48:48 -0700

    Updated source

--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -10,7 +10,7 @@ class ColumbiaStudentResource:
     def _get_connection():
 
         conn = pymysql.connect(
-            user="dbuser",
+            user="root",
             password="dbuserdbuser",
             host="localhost",
             cursorclass=pymysql.cursors.DictCursor,
@@ -21,7 +21,8 @@ class ColumbiaStudentResource:
     @staticmethod
     def get_by_key(key):
 
-        sql = "SELECT * FROM aa_w4111_f22.columbia_student where uni=%s";
+        sql = "SELECT * FROM f22_databases.columbia_students where guid=%s";
+        # sql = "SELECT * FROM f22_databases.columbia_student where uni=%s";
         conn = ColumbiaStudentResource._get_connection()
         cur = conn.cursor()
         res = cur.execute(sql, args=key)
